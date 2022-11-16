133d5d0839f6c9cb61a9860ef51e966c3666f1a5
Luc Randriamanantseheno <luckylucio14@gmail.com>
2022-11-16 16:47:01 +0100

    MVP yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+La Mandarine = {name: "La Mandarine", address: "2 Rue de la Rivaudière, 44800 Saint-Herblain", rating: 3}
+Mia Nonna =  {name: "Mia Nonna", address: "14 rue Beauregard, 44000 Nantes", rating: 4}
+Le Mas des oliviers =  {name: "Le Mas des oliviers", address: "Rue Moulin de la Rousselière, 44800 Saint-Herblain", rating: 5}
+Bombay =  {name: "Bombay", address: "6 B rue kervegan, 44000 Nantes", rating: 4}
+Chez Fruta =  {name: "Chez Fruta", address: "2 rue Copernic, 44000 Nantes", rating: 5}
+Que Me =  {name: "Que Me", address: "15 rue de la Juiverie, 44000 Nantes", rating: 1}
+
+[La_Mandarine, Mia_Nonna, Le_Mas_des_oliviers, Bombay, Chez_Fruta, Que_me].each do |attributes|
+restaurant = Restaurant.create!(attributes)
+puts "Created #{restaurant.name}"
+
+puts 'Finished!'
+end
